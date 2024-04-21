1c69be7fd75c00be72890ecb0e93ce6f5b3883ba
Dinesh Kumar <61492032+choudhary-dinesh@users.noreply.github.com>
2024-04-21 19:51:03 +0530

    Update utils.py

--- bert-base/utils.py
+++ bert-base/utils.py
@@ -23,6 +23,23 @@ def process_data_to_model_inputs_summarization(batch):
   # We have to make sure that the PAD token is ignored
   batch["labels"] = [[-100 if token == tokenizer.pad_token_id else token for token in labels] for labels in batch["labels"]]
   return batch
+  
+def process_data_to_model_inputs_translation(batch):
+  inputs = ["translate English to Hindi: " + ex["en"] for ex in batch["translation"]]
+  targets = [ex["hi"] for ex in batch["translation"]]
+  # tokenize the inputs and labels
+  inputs = tokenizer(inputs, padding="max_length", truncation=True, max_length=encoder_max_length)
+  outputs = tokenizer(targets, padding="max_length", truncation=True, max_length=decoder_max_length)
+  batch["input_ids"] = inputs.input_ids
+  batch["attention_mask"] = inputs.attention_mask
+  batch["decoder_input_ids"] = outputs.input_ids
+  batch["decoder_attention_mask"] = outputs.attention_mask
+  batch["labels"] = outputs.input_ids.copy()
+  # because BERT automatically shifts the labels, the labels correspond exactly to `decoder_input_ids`.
+  # We have to make sure that the PAD token is ignored
+  batch["labels"] = [[-100 if token == tokenizer.pad_token_id else token for token in labels] for labels in batch["labels"]]
+  return batch
+
 
 def compute_metrics(pred):
   labels_ids = pred.label_ids
